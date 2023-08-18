061e69cac07756147417667b311ad83906c3aac2
bmaellet <bmaellet@gmail.com>
2023-08-17 19:28:53 -0700

    Project Debugging: 0x03.C - Debugging: Modified

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
-* main - takes a date and prints how many days are left in the year, taking
-* leap years into account
-* Return: 0
-*/
+ * main - takes a date and prints how many days are left in the year, taking
+ * leap years into account
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -25,3 +25,4 @@ int main(void)
 
     return (0);
 }
+
